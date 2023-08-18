b124d0c1cf3350f48d08997e729482369b8bbd39
Thomas Islas <islas.thomas115@gmail.com>
2023-08-18 20:46:22 -0300

    se arreglo de nuevo el login

---
+++ C.U.K Score/login/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/estilosLogin.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <header>
+        <div class="div-header">
+            <div class="logo">
+                <img src="../img/logoCUK.png" alt="logoCUK" width="100px">
+            </div>
+            <div class="tituloCUK">
+                <h2>
+                    CONFEDERACIÓN URUGUAYA DE KARATE
+                </h2>
+            </div>
+            <div class="volver">
+                <a href="../indexProgramación.html"><img src="../img/inicio.png" alt="inicio" width="50px"></a>
+            </div>
+        </div>
+    </header>
+    <div class="centrar">
+        <div class="body">
+            <div class="container">
+                <form>
+                    <div class="head">
+                        <span>Inicia Sesion</span>
+                        <p>Inicia Sesion para ver lo nuevo de C.U.K Score</p>
+                    </div>
+                    <div class="inputs">
+                        <input type="ci" placeholder="CI">
+                        <input type="password" placeholder="Contraseña">
+                    </div>
+                    <button>Iniciar Sesion</button>
+                </form>
+                <div class="form-footer">
+                    <p>No tienes una cuenta? <a href="registro.php">Registrate</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
