63cbcf8b60f365f5357c4a375898d9684beedfa2
Thomwe <islas.thomas115@gmail.com>
2023-08-22 14:41:25 -0300

    index, login y registro comparten mismo css en C.U.K. Score

--- C.U.K Score/login/registro.php
+++ C.U.K Score/login/registro.php
@@ -4,48 +4,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/estilosLogin.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <title>Login</title>
 </head>
 
 <body>
+
+    <!-- Inicio Header -->
+
     <header>
-        <div class="div-header">
-            <div class="logo">
-                <img src="../img/logoCUK.png" alt="logoCUK" width="100px">
-            </div>
-            <div class="tituloCUK">
-                <h2>
-                    CONFEDERACIÓN URUGUAYA DE KARATE
-                </h2>
-            </div>
-            <div class="volver">
-                <a href="../index.html"><img src="../img/inicio.png" alt="inicio" width="50px"></a>
-            </div>
-        </div>
+        <a href="#" class="logo">
+            <img src="../img/logoCUK.png" alt="logo C.U.K." /">
+            <h2 class="nombreCUK">C.U.K. Score</h2>
+        </a>
+        <nav class="links">
+            <a href="../index.html">
+                <img src="../img/inicio.png" alt="boton de inicio" / width="40em">
+            </a>
+        </nav>
     </header>
-    <div class="centrar">
-        <div class="body">
-            <div class="contenedor-principal">
-                <form>
-                    <div class="formularios">
-                        <span>Registrarse</span>
-                        <p>Forma parte de C.U.K Score</p>
+
+    <!-- Fin Header -->
+
+    <!-- Inicio del formulario Registro-->
+    <section>
+        <div class="centrar">
+            <div class="body">
+                <div class="contenedor-principal">
+                    <form>
+                        <div class="formularios">
+                            <span>Registrarse</span>
+                            <p>Forma parte de C.U.K Score</p>
+                        </div>
+                        <div class="inputs">
+                            <input type="text" placeholder="Nombre Completo">
+                            <input type="email" placeholder="Email">
+                            <input type="ci" placeholder="CI">
+                            <input type="password" placeholder="Contraseña">
+                        </div>
+                        <button>Registrarse</button>
+                    </form>
+                    <div class="footer-formularios">
+                        <p>Tienes una cuenta? <a href="login.php">Inicia Sesion</a></p>
                     </div>
-                    <div class="inputs">
-                        <input type="text" placeholder="Nombre Completo">
-                        <input type="email" placeholder="Email">
-                        <input type="ci" placeholder="CI">
-                        <input type="password" placeholder="Contraseña">
-                    </div>
-                    <button>Registrarse</button>
-                </form>
-                <div class="footer-formularios">
-                    <p>Tienes una cuenta? <a href="login.php">Inicia Sesion</a></p>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+
+    <!-- Fin del formulario Registro-->
 </body>
 
 </html>
