de2e55373461bd6f08c84e301be8174c150b616b
Martin Leites <martinwleites@gmail.com>
2023-08-23 21:02:01 -0300

    se arreglo un pequeño error de carpetas

--- C.U.K Score/login/validar.php
+++ C.U.K Score/login/validar.php
@@ -1,27 +1,25 @@
 <?php
-$usuario=$_POST['usuario'];
-$contraseña=$_POST['contraseña'];
+$usuario = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
 session_start();
-$_SESSION['usuario']=$usuario;
+$_SESSION['usuario'] = $usuario;
 
 include('db.php');
 
-$consulta="SELECT*FROM usuario where usuario='$usuario' and contraseña='$contraseña'";
-$resultado=mysqli_query($conexion,$consulta);
+$consulta = "SELECT*FROM usuario where usuario='$usuario' and contraseña='$contraseña'";
+$resultado = mysqli_query($conexion, $consulta);
 
-$filas=mysqli_num_rows($resultado);
-if($filas){
+$filas = mysqli_num_rows($resultado);
+if ($filas) {
     header("location:homeCoach.php");
-
-}else{
-    ?>
+} else {
+?>
     <?php
     include("index.php");
     ?>
     <h1>ERROR EN LA AUTENTIFICACION</h1>
-    <?php
+<?php
 }
 mysqli_free_result($resultado);
 mysqli_close($conexion);
 ?>
-
